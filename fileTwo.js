9d4e5c2e940126ea12edfebac25a384856fa9fef
Sam Schantz <samvschantz@gmail.com>
2018-03-19 19:04:00 +0000

    Changed fileTwo to export function

--- fileTwo.js
+++ fileTwo.js
@@ -1,27 +1,27 @@
 var https = require('https');
 
-function getAndPrintHTML () {
+module.exports = {
+  getAndPrintHTML: function getAndPrintHTML () {
 
-  var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step2.html'
-  };
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step2.html'
+    };
 
-  var bufferVar = ''
+    var bufferVar = ''
 
-  https.get(requestOptions, function(response){
+    https.get(requestOptions, function(response){
 
-    response.setEncoding('utf8');
+      response.setEncoding('utf8');
 
-    response.on('data', function (data) {
+      response.on('data', function (data) {
 
-      bufferVar += data
-      console.log(bufferVar)
+        bufferVar += data
+        console.log(bufferVar)
 
+      });
     });
-  });
+  }
 }
 
-
-
-getAndPrintHTML()
+module.exports['getAndPrintHTML']()
